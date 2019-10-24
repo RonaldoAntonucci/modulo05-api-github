90adf33dcd0e8918e3f1a7632554d1d641b6b8a9
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-10-24 16:22:40 -0300

    finalizada de acordo com as aulas

---
+++ src/pages/Main/index.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
+
+export default function Main() {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(Boolean);
+
+  useEffect(() => {
+    const loadRep = localStorage.getItem('repositories');
+
+    if (repositories) {
+      setRepositories(JSON.parse(loadRep));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('repositories', JSON.stringify(repositories));
+  }, [repositories]);
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const repExist = repositories.find(rep => rep.name === newRepo);
+
+    if (repExist) return;
+
+    setLoading(true);
+
+    await api
+      .get(`/repos/${newRepo}`)
+      .then(res => {
+        const data = {
+          name: res.data.full_name,
+        };
+
+        setRepositories([...repositories, data]);
+        setNewRepo('');
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }
+
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
+
+        <SubmitButton loading={loading}>
+          {loading ? (
+            <FaSpinner color="#FFF" size={14} />
+          ) : (
+            <FaPlus color="#FFF" size={14} />
+          )}
+        </SubmitButton>
+      </Form>
+      <List>
+        {repositories.map(repository => (
+          <li key={repository.name}>
+            <span>{repository.name}</span>
+            <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+              Detalhes
+            </Link>
+          </li>
+        ))}
+      </List>
+    </Container>
+  );
+}
